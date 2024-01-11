7abde75678797c9d56e12ca8ab224dea4a707d55
Tonya <tonya.brunetti@colorado.edu>
2024-01-11 11:50:37 -0700

    added comments to code steps

--- scripts/fgsea_cNMF_ranks_run_script.R
+++ scripts/fgsea_cNMF_ranks_run_script.R
@@ -14,21 +14,31 @@ nes_thresh = "positive" # options are positive, negative, or both
 ##########################################################################################
 
 
-# Step 1 - calulate your enrichments scores using fgsea (ONLY NEEDS TO BE DONE ONCE FOR A DATASET!)
+
+##########################################################################################
+################## Step 1 - calculate your enrichment scores using fgsea #################
+### ONLY NEEDS TO BE DONE ONCE FOR A DATASET, as data is save as .Rdat for use later! ####
+##########################################################################################
 step1_enrichment_results <- calculate_enrichment(gmt_file_input = gmt_file_input, 
                                                  output_prefix = output_prefix, 
                                                  cnmf_spectra_scores_file = cnmf_spectra_scores_file, 
                                                  seed = seed)
 
-
-# Step 2 - filter out your results
+##########################################################################################
+###################### Step 2 - filter fGSEA results by padj and NES #####################
+##########################################################################################
 filtered_fgsea_results <- filter_results(unfiltered_fgsea_results = step1_enrichment_results@unfiltered, 
                                          adjp_thresh = adjp_thresh, 
                                          nes_thresh = nes_thresh)
+
 # if you want to look at your results as a dataframe, you can just put your GEP id in the key
+# for example, if you wanted to store the GEP1 results as a dataframe you could run the following:
 myGep1_results <- filtered_fgsea_results[["1"]]
 
-# Step 3 - plot results and get leading edge genes
+
+##########################################################################################
+###################### Step 3 - visualization and leading edge genes #####################
+##########################################################################################
 # plot the top 10 NES pathways on your filtered data for GEP 1
 table_summary_plot(filtered_results = filtered_fgsea_results, 
                    gmt_large_list = step1_enrichment_results@gmt_large_list, 
